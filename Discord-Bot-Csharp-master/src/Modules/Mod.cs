38244ae3bb1cafecc6629d46827174b6d0911a5f
Ryan <45146085+pl1120@users.noreply.github.com>
2022-11-09 03:10:55 +0000

    Add files via upload

---
+++ Discord-Bot-Csharp-master/src/Modules/Mod.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace Discord_Bot
+{
+    public class Mod : ModuleBase<SocketCommandContext>
+    {
+        [Command("kick")]
+        [Summary("Kick a user from the server.")]
+        [RequireBotPermission(GuildPermission.KickMembers)]
+        [RequireUserPermission(GuildPermission.KickMembers)]
+        public async Task Kick(SocketGuildUser targetUser, [Remainder] string reason = "No reason provided.")
+        {
+            await targetUser.KickAsync(reason);
+            await ReplyAsync($"**{targetUser}** has been kicked. Bye bye :wave:");
+        }
+
+        [Command("reloadconfig")]
+        [Summary("Reloads the config and applies changes")] 
+        [RequireOwner] // Require the bot owner to execute the command successfully.
+        public async Task ReloadConfig()
+        {
+            await Functions.SetBotStatusAsync(Context.Client);
+            await ReplyAsync("Reloaded!");
+        }
+    }
+}
